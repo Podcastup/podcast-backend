b2b41f1df6be9a97602c71359f536fe8e78f1385
myrdstom <nserekopaul@gmail.com>
2020-05-24 03:50:05 +0300

    Setup User endpoints #13

--- api/src/routes/api/index.ts
+++ api/src/routes/api/index.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express';
 
 import adminRoute from './admins.route';
+import userRoute from './users.route';
 
 
 const routes = Router();
 
 routes.use('/', adminRoute);
+routes.use('/', userRoute);
 
 export default routes;
---
+++ api/src/routes/api/users.route.ts
@@ -0,0 +1,53 @@
+import {celebrate, Joi, Segments} from "celebrate";
+import {NextFunction, Request, Response, Router} from "express";
+import passport from 'passport';
+
+import {IRequest, userController} from "../../controllers/user-controller";
+
+const router = Router();
+
+router.get(
+    "/user/login/:token",
+    (req: Request, res: Response, next: NextFunction) => userController.getLoggedInUser(req, res),
+);
+
+router.post(
+    "/user",
+    celebrate({
+        [Segments.BODY]: Joi.object().keys({
+            email: Joi.string().required().email(),
+            firstName: Joi.string().required(),
+            lastName: Joi.string().required()
+        }),
+    }),
+    (req:Request, res:Response, next:NextFunction) => userController.createRecord(req,res),
+);
+
+router.post(
+    "/user/login",
+    (req:Request, res:Response, next:NextFunction) => userController.loginUser(req,res),
+);
+
+router.get(
+    "/user",
+    (req: Request, res: Response, next: NextFunction) => userController.findAllRecords(req, res),
+);
+
+router.get(
+    "/user/:id",
+    (req: Request, res: Response, next: NextFunction) => userController.findOneRecord(req, res),
+);
+
+router.delete(
+    '/user/:id',
+    passport.authenticate('jwt', { session: false }, null),
+    (req:Request, res:Response, next:NextFunction) => userController.deleteRecord(req, res),
+);
+
+router.put(
+    "/user/:id",
+    passport.authenticate('jwt', { session: false }, null),
+    (req:Request, res:Response, next:NextFunction) => userController.updateRecord(req, res),
+);
+
+export default router
